02d19ace152b4477c7b9126697d0af31a50ce7f3
Koorosh Gobal <kooroshg1@gmail.com>
2015-11-21 12:09:09 -0500

    Working code for solving M-K system with FFT

--- Matlab_files/solve_MK_with_fft.m
+++ Matlab_files/solve_MK_with_fft.m
@@ -3,18 +3,21 @@ clear all;
 close all;
 format short g;
 % ------------------------------------------------------------------------------------------------------------------------------ %
-N = 59;
-t = linspace(0, 2*pi, N+1)'; 
-omega = 2 * pi / (t(end) - t(1));
+N = 59; % Number of sample points in time
+t = linspace(0, 2*pi, N+1)';  % Time
+omega = 2 * pi / (t(end) - t(1)); % delta f
 t = t(1:end-1);
-f = sin(2*t);
+f = sin(2*t) + cos(3*t); % Forcing functions
 
 F = round(fft(f));
 
 Omega = omega * [0,-1:-1:floor(-N/2),floor(N/2-1):-1:1]' + eps;
 
-X = F ./ (1 - Omega.^2);
+X = F ./ (1 - Omega.^2); % Solution in frequency domain
 x = ifft(X)
 figure,
-plot(t, x,...
-       t , -1/3 * sin(2 * t))
+plot(t, x, 'k', ...
+       t , -1/3 * sin(2 * t) - 1/8 * cos(3*t), 'r')
+xlabel('Time')
+ylabel('Displacement')
+legend('FFT Solution', 'Analytical Solution')
