937e7fb62472042c63747b2f3e9c0c0d2dedc238
kooroshg1 <kooroshg1@gmail.com>
2015-11-23 15:40:03 -0500

    Fixed optimization problem definition

--- Python_files/Minimize_the_residual/main.py
+++ Python_files/Minimize_the_residual/main.py
@@ -28,22 +28,25 @@ from scipy.integrate import odeint
 # plt.show()
 
 # # \ddot{x} + \dot{x} + x = sin(2*t)
-N = 99
+N = 19
 t = np.linspace(0, 2*np.pi, N+1)
 t = t[0:-1]
 f = np.sin(2*t)
 F = np.fft.fft(f)
 Omega = np.fft.fftfreq(N, 1/N) + 0.00001
 x0 = np.ones(N)
+# x0 = f
 xAnalytical = -0.23077 * np.sin(2*t) -0.15385 * np.cos(2*t)
 def residual(x):
     X = np.fft.fft(x)
     ddx = np.fft.ifft(np.multiply(-Omega**2, X))
     dx = np.fft.ifft(np.multiply(1j * Omega, X))
     R = ddx + dx + x - f
-    R = np.sum(np.abs(R))
+    # R = np.sum(np.abs(np.real(R)))
+    R = np.sum(np.abs((R**2)))
     return R
 
+# res = minimize(residual, x0, options={'method':'SLSQP', 'maxiter':100000})
 res = minimize(residual, x0)
 print(residual(res.x))
 plt.figure()
@@ -51,7 +54,7 @@ plt.plot(t, res.x, 'k',
          t, xAnalytical, 'ro')
 plt.legend(['FFt', 'Analytical'])
 plt.show()
-print(res.x)
+print(res.jac)
 
 # # \ddot{x} + x - x**3 = sin(2*t)
 # N = 99
