d6a663536bba429204721f3de397e8f097f9c1ce
Samyu-Vana <nuthy.2@wright.edu>
2015-12-15 12:47:50 -0500

    updating linear code

--- Matlab_files/myfun.m
+++ Matlab_files/myfun.m
@@ -5,13 +5,13 @@ close all
 clear all
 
 %ddx + x = sin(2*t)
-
-N = 99;
-w = 2
-T = 6*1*pi;
+w = 2;
+f = w/(2*pi);
+N = 9;
+T = 1/f;
 t = linspace(0, T, N+1);
-t = t(1:end-1)';
 omega = 2 * pi / (t(end) - t(1));
+t = t(1:end-1)';
 
 Omega = omega*[0,-1:-1:floor(-N/2),floor(N/2-1):-1:1]'+eps;
 
@@ -19,24 +19,26 @@ Omega = omega*[0,-1:-1:floor(-N/2),floor(N/2-1):-1:1]'+eps;
 
 F = fft(f);
 
-X_H = F./(1-Omega.^2);
-x_h = ifft(X_H);
+% X_H = F./(1-Omega.^2);
+% x_h = ifft(X_H);
 
-x0 = 0*ones(N,1);
+x0 = 1*ones(N,1);
 
-X1 = fminsearch(@objfun,0);
+[X1,fval] = fminsearch(@objfun,x0);
 
 function error = objfun(x)
      X = fft(x);
  ddx = ifft(-Omega.^2.*X);
 
- 
  RES = (((ddx + x - f)));
- error = sum((RES).^2);
+ error = sum(abs(RES).^2);
 end
 
  figure(1)
  xAnalytical = -sin(2.*t) / 3;
- plot(t,X1,t,xAnalytical, 'ro',t,x_h,'k-');
+ size(X1)
+% abs([X1 x_h xAnalytical])
+%  plot(t,X1,t,xAnalytical, 'ro',t,x_h,'k-');
+plot(t,X1,t,xAnalytical, 'ro');
  
  end
